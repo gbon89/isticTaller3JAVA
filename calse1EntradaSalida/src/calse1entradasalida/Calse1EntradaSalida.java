a6d5936ee2d3e94f8100751549ee3eba564d65d8
gbon89 <48774625+gbon89@users.noreply.github.com>
2019-03-20 19:59:40 -0300

    Primer ejemplo de entrada y concatenación
    Primer ejemplo de entrada y concatenación

---
+++ calse1EntradaSalida/src/calse1entradasalida/Calse1EntradaSalida.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calse1entradasalida;
+
+/**
+ *
+ * @author PC
+ */
+public class Calse1EntradaSalida {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");
+        int edad;
+        edad = 33; 
+        System.out.println("Su edad es " + edad);
+    }
+    
+}
